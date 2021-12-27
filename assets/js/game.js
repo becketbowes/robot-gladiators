41b535c46a376a2b1b7f40c6571c8f9966916b85
becket <becket.bowes@gmail.com>
2021-12-26 20:41:19 -0500

    changed shopping options to numeric values

--- assets/js/game.js
+++ assets/js/game.js
@@ -140,29 +140,25 @@ var endGame = function() {
 var shop = function() {
   // ask player what they'd like to do
   var shopOptionPrompt = window.prompt(
-    'Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one "REFILL", "UPGRADE", or "LEAVE" to make a choice.'
+    'Welcome to the shop. Press 1 to refill your health, 2 to upgrade your attack, or 3 to leave.'
   );
 
+  shopOptionPrompt = parseInt(shopOptionPrompt);
+
+  debugger;
   // use switch case to carry out action
   switch (shopOptionPrompt) {
-    case 'REFILL':
-    case 'refill':
+    case 1:
       playerInfo.refillHealth();
       break;
-    case 'UPGRADE':
-    case 'upgrade':
+    case 2:
       playerInfo.upgradeAttack();
       break;
-    case 'LEAVE':
-    case 'leave':
+    case 3:
       window.alert('Leaving the store.');
-
-      // do nothing, so function will end
       break;
     default:
-      window.alert('You did not pick a valid option. Try again.');
-
-      // call shop() again to force player to pick a valid option
+      window.alert('You did not pick a valid option. Please try again.');
       shop();
       break;
   }
